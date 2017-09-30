25e44111adb03224c3451a7f8ec807cce9886453
dailybp <bpdailey@gmail.com>
2017-09-30 11:08:08 -0400

    reverted back to older code

--- assets/js/new_script.js
+++ assets/js/new_script.js
@@ -1,37 +1,21 @@
-var config = {
-    apiKey: "AIzaSyBf6G5CDw1GNdhHTUUp8oOmgY9sPQxUCQM",
-    authDomain: "dealio-8c33b.firebaseapp.com",
-    databaseURL: "https://dealio-8c33b.firebaseio.com",
-    projectId: "dealio-8c33b",
-    storageBucket: "dealio-8c33b.appspot.com",
-    messagingSenderId: "260259922331"
-  };
-  firebase.initializeApp(config);
-
-var dataRef = firebase.database();
-
-console.log(dataRef);
-
-//Yelp API
-$.post("https://api.yelp.com/oauth2/token",{
-  grant_type: "client_credentials",
-  client_id: "l_OltA5Ew4ICDizscxfjeg",
-  client_secret: "yfDHBhk1vmpuW5BiOj5nqXFGKT920vVSf011iddUwUcyPKF7qWyIRkTZWVWXDqhV"
-}, function(response){
-  console.log(response);
-  var token = response.access_token;
+//$.post("https://api.yelp.com/oauth2/token",{
+//  grant_type: "client_credentials",
+ // client_id: "l_OltA5Ew4ICDizscxfjeg",
+ // client_secret: "yfDHBhk1vmpuW5BiOj5nqXFGKT920vVSf011iddUwUcyPKF7qWyIRkTZWVWXDqhV"
+//}, function(response){
+//  console.log(response);
+//  var token = response.access_token;
   //write all js in here (yelp)
 
-  var yelpQueryURL = "https://api.yelp.com/v3/businesses/search?location=" + zipCode + "&ll" + latitude + "," + longitude;
+
   //--------
-  $.ajax({
-    url: yelpQueryURL,
-    headers:{"Authorization": "Bearer " + token}
-  }).done(function(response){
-    console.log(response);
-  });
+ // $.ajax({
+ //   url: "https://api.yelp.com/v3/businesses/search?location=Washington+DC",
+ //   headers:{"Authorization": "Bearer " + token}
+ // }).done(function(response){
+//    console.log(response);
+//  });
   //responses received from
-  //$("#reviews-View").html()
 
 $(function(){
     var select=document.getElementById("deal-category");
@@ -112,22 +96,8 @@ console.log(onclick="window.location.href=coupon.url");
 
 }
 
-  $("#deals-View").html(sqootData);//populates the secondary html page with sqootData deals
+//---Shows Sqoot API Data--
+    console.log(sqootData);
 
-  // function storeToFirebase(){
-  //   dataRef.ref().push(sqootData);
-  // }
-
-  console.log(sqootData);
-  for(var i = 0; i < sqootData.deals.length; i++){
-    phoneNumber = sqootData.deals[i].deal.merchant.phone_number;
-    console.log(phoneNumber);
-    zipCode = sqootData.deals[i].deal.merchant.postal_code;
-    console.log(zipCode);
-    latitude = sqootData.deals[i].deal.merchant.latitude;
-    console.log(latitude);
-    longitude = sqootData.deals[i].deal.merchant.longitude;
-    console.log(longitude);
-  }
-
-});
+    })
+  // });
